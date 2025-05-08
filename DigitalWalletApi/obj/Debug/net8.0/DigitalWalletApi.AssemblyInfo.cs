7c2dad52fcb9152a41111149bf25f18a76737396
EduGollner0609 <dudugollner@gmail.com>
2025-05-08 01:48:46 -0300

    Adiconando pacotes do entity e criando stringConnection.

--- DigitalWalletApi/obj/Debug/net8.0/DigitalWalletApi.AssemblyInfo.cs
+++ DigitalWalletApi/obj/Debug/net8.0/DigitalWalletApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a097b7519b14ffc7de983793b2912f8f65c2a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0872f4227b96bebd886864518fda4419cb389d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
