322b4a1c9b8addff096b91584b3edfbe93013619
EduGollner0609 <dudugollner@gmail.com>
2025-05-08 03:16:12 -0300

    Criando TokenService com metódo para geração de token.

---
+++ DigitalWalletApi/Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using DigitalWalletApi.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DigitalWalletApi.Services
+{
+    public static class TokenService
+    {
+        public static string GeneratedToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Email.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- DigitalWalletApi/obj/Debug/net8.0/DigitalWalletApi.AssemblyInfo.cs
+++ DigitalWalletApi/obj/Debug/net8.0/DigitalWalletApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41ca964779abeed65ee6737b4b7daa2a263f6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad56a309fae2a519c6695e8b13594b1282601b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalWalletApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
