879b7918682b84bcad9e089dd26a9e28fa844b8e
EduGollner0609 <dudugollner@gmail.com>
2025-05-15 11:45:21 -0300

    Removendo atributos desnecessarios.

--- DigitalWallet.Application/UseCases/Wallet/Commands/Deposit/DepositResponse.cs
+++ DigitalWallet.Application/UseCases/Wallet/Commands/Deposit/DepositResponse.cs
@@ -1,14 +1,10 @@
 ﻿using UserModel = DigitalWallet.Domain.Domain.Entities.User;
-using DigitalWallet.Application.UseCases.DTOs;
 using DigitalWallet.Application.UseCases.DTOs.Abstractions;
 
 namespace DigitalWallet.Application.UseCases.Wallet.Commands.Deposit
 {
     public class DepositResponse : WalletResponseDTO
     {
-        public UserSimpleDTO User { get; private set; }
-        public decimal NewBalance { get; private set; }
-
         public DepositResponse(UserModel user) : base(user) { }
     }
 }
--- DigitalWallet.Application/UseCases/Wallet/Queries/GetBalance/GetBalanceResponse.cs
+++ DigitalWallet.Application/UseCases/Wallet/Queries/GetBalance/GetBalanceResponse.cs
@@ -1,14 +1,10 @@
 ﻿using UserModel = DigitalWallet.Domain.Domain.Entities.User;
-using DigitalWallet.Application.UseCases.DTOs;
 using DigitalWallet.Application.UseCases.DTOs.Abstractions;
 
 namespace DigitalWallet.Application.UseCases.Wallet.Queries.GetBalance
 {
     public class GetBalanceResponse : WalletResponseDTO
     {
-        public UserSimpleDTO User { get; private set; }
-        public decimal Balance { get; private set; }
-
         public GetBalanceResponse(UserModel user) : base(user) { }
     }
 }
